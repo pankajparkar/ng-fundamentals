5fd9760f8d057c2f4d94ee9305a31dded3373344
Pankaj Parkar <parkarpankaj3@gmail.com>
2024-10-11 23:52:53 +0800

    added navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from "./components/navbar/navbar.component";
 
 @Component({
   selector: 'nf-root',
   standalone: true,
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
+  imports: [NavbarComponent]
 })
 export class AppComponent {
   title = 'ng-fundamentals';
---
+++ src/app/components/navbar/navbar.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nf-navbar',
+  standalone: true,
+  imports: [],
+  templateUrl: './navbar.component.html',
+  styleUrl: './navbar.component.scss'
+})
+export class NavbarComponent {
+
+}
