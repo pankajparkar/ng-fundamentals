b0c210207e5f42ee9006803b518dff8f01914894
Pankaj Parkar <parkarpankaj3@gmail.com>
2024-10-11 23:44:50 +0800

    remove router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'nf-root',
   standalone: true,
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
