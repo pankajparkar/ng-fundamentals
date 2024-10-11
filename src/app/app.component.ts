ea8def524298b20efd4c3c54325dc8a7070d6477
Pankaj Parkar <parkarpankaj3@gmail.com>
2024-10-11 23:55:56 +0800

    added user list component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { NavbarComponent } from "./components/navbar/navbar.component";
+import { UserListComponent } from "./components/user-list.component";
 
 @Component({
   selector: 'nf-root',
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
-  imports: [NavbarComponent]
+  imports: [NavbarComponent, UserListComponent]
 })
 export class AppComponent {
   title = 'ng-fundamentals';
---
+++ src/app/components/user-list.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'nf-user-list',
+  standalone: true,
+  imports: [],
+  template: `
+    <h2></h2>
+    <ul>
+      <li>User 1</li>
+      <li>User 2</li>
+      <li>User 3</li>
+      <li>User 4</li>
+    </ul>
+  `,
+  styles: ``
+})
+export class UserListComponent {
+
+}
